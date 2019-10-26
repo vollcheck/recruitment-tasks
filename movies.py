1e1eed6c26c13556fe79de1414815de5ccdc0600
j3k-w <j3k.walczak@gmail.com>
2019-10-26 22:54:19 +0200

    testing; first steps to insert data into db #2

--- movies.py
+++ movies.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
 import sqlite3
 import requests
-
+import json
 
 if __name__ == "__main__":
+    # CONST
     API_KEY = '37ac9525'
 
+    # DB CONNECTION
     conn = sqlite3.connect('movies.sqlite')
     c = conn.cursor()
     c.execute('''SELECT title FROM movies''')
-
     con = c.fetchone()[0]
 
+    # RESPONSE
     link = f'http://www.omdbapi.com/?t={con}&apikey={API_KEY}'
     response = requests.get(link)
 
-    print(response.status_code)
-    jr = response.json()
-    print(jr['Year'])
+    # TESTING
+    # jr = response.json()
+    downloaded = json.dumps(response.json())
+    print(downloaded)
+    #c.execute('''INSERT INTO movies ''')
+    # print(type(response.json()))
 
+
+    # CONVERT JSON TO SQLITE
+    columns = ['id', 'title', 'year', 'runtime',
+               'genre', 'director', 'cast', 'writer',
+               'language', 'country', 'awards',
+               'imdb_rating', 'imdb_votes', 'box_office']
+
+    # END
     conn.close()
 
     """
