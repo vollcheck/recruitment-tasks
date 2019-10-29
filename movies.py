b685657ea6005838b7b4060ac3f139267ba6669b
j3k <j3k.walczak@gmail.com>
2019-10-29 09:08:43 +0100

    open and close db

--- movies.py
+++ movies.py
@@ -3,58 +3,44 @@ import requests
 import sqlite3
 from sqlite3 import Error
 import argparse
-
+import contextlib
 
 class DataLead():
-    def __init__(self, db, API_KEY, args):
-        self.db = db
+    def __init__(self, dbname, API_KEY, args):
+        self.dbname = dbname
         self.API_KEY = API_KEY
         self.args = args
+        self.db = self.create_connection()
 
-    def download_single_movie(self):
+    def create_connection(self):
+        conn = None
         try:
-            conn = sqlite3.connect(self.db)
+            conn = sqlite3.connect(self.dbname)
             print("Database is ready!")
         except Error as e:
             print(e)
 
-        # Make a cursor for playing with db
-        c = conn.cursor()
-        c.execute('''SELECT title FROM movies WHERE id=1;''')
-        title = c.fetchall()
+        return conn
 
-        # Requests response
-        link = f'http://www.omdbapi.com/?t={title}&apikey={self.API_KEY}'
-        content = requests.get(link).json()
+    def do_sth(self):
+        print("This is test function")
+        print(f"arg for that object is {self.args}")
 
-        columns = ['Title', 'Year', 'Runtime',
-                   'Genre', 'Director', 'Actors', 'Writer',
-                   'Language', 'Country', 'Awards',
-                   'imdbRating', 'imdbVotes', 'BoxOffice']
-
-        dt = [content[col] for col in columns]
-
-        print(dt)
-
-        # query = '''UPDATE MOVIES ('Title', 'Year', 'Runtime',
-        # 'Genre', 'Director', 'Cast', 'Writer',
-        # 'Language', 'Country', 'Awards',
-        # 'imdb_rating', 'imdb_votes', 'Box_office')
-        # VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
-        # WHERE TITLE="The Shawshank Redemption"; '''
-
-        # https://www.w3schools.com/sql/sql_update.asp
-
-        # # conn.commit()
-        # # END
-        conn.close()
+    def close(self):
+        if self.db:
+            self.db.close()
+            self.db = None
+            print("The database connection has been closed.")
 
 
 if __name__ == "__main__":
     API_KEY = '37ac9525'
 
-    DL = DataLead('movies.sqlite', API_KEY, 'columns')
-    DL.download_single_movie()
+    #DL = DataLead('movies.sqlite', API_KEY, 'randomargumentjustfortest')
+    with contextlib.closing(DataLead('movies.sqlite', API_KEY, 'randomargumentjustfortest')) as DL:
+        DL.do_sth()
+    #DL.create_connection()
+
 
     # parser = argparse.ArgumentParser()
     # parser.add_argument("--sort_by", help="sort movies by columns")
