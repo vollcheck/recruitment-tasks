d371bb920b20cb47359d8353228188833c1a5772
j3k <j3k.walczak@gmail.com>
2019-10-29 19:39:16 +0100

    successfully download movie given by command

--- movies.py
+++ movies.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import sqlite3
 from sqlite3 import Error
-import requests, argparse, contextlib
+import requests
+import argparse
+import contextlib
 
 
 class DataLead():
@@ -11,27 +13,32 @@ class DataLead():
         self.db = self.open_connection()
 
     def open_connection(self):
-        conn = None
+        db = None
         try:
-            conn = sqlite3.connect(self.dbname)
-            print("Database is ready to go!")
+            db = sqlite3.connect(self.dbname)
+            print("-- Database is ready to go!")
         except Error as e:
             print(e)
 
-        return conn
+        return db
 
-    def do_sth(self):
-        print("do_sth function is printing")
-        c = self.db.cursor()
-        c.execute('''select * from movies where id=1''')
-        title = c.fetchall()[0]
-        print(title)
+    def download_single_movie(self, _title):
+        print(f"The '{_title}' movie will be downloaded now...")
+        link = f'http://www.omdbapi.com/?t={_title}&apikey={self.API_KEY}'
+        content = requests.get(link).json()
+        columns = ['Title', 'Year', 'Runtime',
+                   'Genre', 'Director', 'Actors', 'Writer',
+                   'Language', 'Country', 'Awards',
+                   'imdbRating', 'imdbVotes', 'BoxOffice']
+
+        dt = [content[col] for col in columns]
+        print(dt)
 
     def close(self):
         if self.db:
             self.db.close()
             self.db = None
-            print("The database connection has been closed.")
+            print("-- The database connection has been closed!")
 
 
 if __name__ == "__main__":
@@ -39,14 +46,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--sort_by", help="sort movies by columns")
+    parser.add_argument("--download_single", help="download only one movie")
     args = parser.parse_args()
 
     with contextlib.closing(DataLead('movies.sqlite', API_KEY)) as DL:
         if not any(vars(args).values()):
             print("There are no arguments passed222!")
-        elif args.sort_by:
-            print("Sorted films go as follow...")
-            DL.do_sth()
-
-    #DL.create_connection()
-    #DL = DataLead('movies.sqlite', API_KEY, 'randomargumentjustfortest')
+        elif args.download_single:
+            DL.download_single_movie(args.download_single)
