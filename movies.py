54435094ddf8750790339acd43dc37fad483de43
j3k <j3k.walczak@gmail.com>
2019-11-02 17:03:05 +0100

    minor changes with class' cursor

--- movies.py
+++ movies.py
@@ -11,8 +11,11 @@ class DataLead():
         self.dbname = dbname
         self.API_KEY = API_KEY
         self.db = self.open_connection()
+        self.c = self.db.cursor()
 
     def open_connection(self):
+        """Opens a connection with database for further data operations."""
+
         db = None
         try:
             db = sqlite3.connect(self.dbname)
@@ -23,15 +26,18 @@ class DataLead():
         return db
 
     def json_from_api(self, title_):
+        """Downloads the JSON movie's data from API with given title."""
+
         print(f"The '{title_}' movie is being downloaded now...")
         link = f'http://www.omdbapi.com/?t={title_}&apikey={self.API_KEY}'
         content = requests.get(link).json()
+
         # Pull the data from JSON
         columns = ['Year', 'Runtime', 'Genre', 'Director', 'Actors', 'Writer',
                    'Language', 'Country', 'Awards', 'imdbRating',
                    'imdbVotes', 'BoxOffice', 'Title']
-        # dt = [content[col] for col in columns]
 
+        # dt = [content[col] for col in columns]
         dt = []
         for col in columns:
             # Ben Hur case, when there is no BoxOffice column
@@ -41,21 +47,22 @@ class DataLead():
                 dt.append(None)
         return dt
 
-    def download_single_movie(self, title):
+    def update_single_movie(self, title):
+        """Function is made for easier development process."""
+
         # Pull out the title from db to check if the movie is in the base
-        c = self.db.cursor()
-        c.execute("select title from movies where title=?", (title,))
-        title = c.fetchone()[0]
+        self.c.execute("select title from movies where title=?", (title,))
+        title = self.c.fetchone()[0]
 
         dt = self.json_from_api(title)
 
-        print(dt)
+        print(dt)  # Testing purposes
 
         # Updating DB
         query_update = '''update movies set year=?, runtime=?, genre=?,
         director=?, cast=?, writer=?, language=?, country=?, awards=?,
         imdb_rating=?, imdb_votes=?, box_office=? where title=?'''
-        c.execute(query_update, dt)
+        self.c.execute(query_update, dt)
         self.db.commit()
 
         # Checking the newly added record
@@ -65,9 +72,8 @@ class DataLead():
         print(f"The '{title}' movie has been downloaded.")
 
     def download_all_movies(self):
-        c = self.db.cursor()
-        c.execute("select title from movies")
-        all_nested = c.fetchall()
+        """Function updating data about all movies appearing in the database."""
+        all_nested = self.c.execute("select title from movies").fetchall()
 
         # Flatten the list of titles
         all_films = [film for sub in all_nested for film in sub]
@@ -80,16 +86,15 @@ class DataLead():
         director=?, cast=?, writer=?, language=?, country=?, awards=?,
         imdb_rating=?, imdb_votes=?, box_office=? where title=?'''
 
-        c.executemany(query_update, films_data)
-
+        self.c.executemany(query_update, films_data)
         self.db.commit()
         print("---> Database has been populated!")
-        return c.lastrowid
+        return self.c.lastrowid
 
     def sort_by(self, params):
-        c = self.db.cursor()
+        # c = self.db.cursor()
         query = f'select title, {params} from movies order by {params}'
-        result = c.execute(query).fetchall()
+        result = self.c.execute(query).fetchall()
         for row in result:
             print(f'{row[0]:<38} {row[1]}')
 
@@ -100,9 +105,9 @@ class DataLead():
         pass
 
     def add(self, title):
-        c = self.db.cursor()
+        "Adds a data of the movie with given title."
         query_select = f"""select * from movies where title='{title}'"""
-        result = c.execute(query_select).fetchall()
+        result = self.c.execute(query_select).fetchall()
 
         if not result:
             dt = self.json_from_api(title)
@@ -113,13 +118,12 @@ class DataLead():
             director, cast, writer, language, country, awards, imdb_rating,
             imdb_votes, box_office) values(?,?,?,?,?,?,?,?,?,?,?,?,?)'''
 
-            c.execute(query_update, dt)
+            self.c.execute(query_update, dt)
             self.db.commit()
             print(f"The '{title}' movie has been downloaded.")
         else:
             print("The title is already in the database!\nDownloading aborted.")
-
-        return c.lastrowid
+        return self.c.lastrowid
 
     def highscores(self):
         pass
@@ -136,10 +140,11 @@ if __name__ == "__main__":
     DB_NAME = 'movies.sqlite'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--download_single", help="download one movie")
+    parser.add_argument("--update_single", help="update one movie")
     parser.add_argument("--download_all", action='store_true', help="download all movies")
     parser.add_argument("--sort_by", help="sort movies by columns")
     parser.add_argument("--add", help="download and add a movie to the db")
+    parser.add_argument("--highscores", action='store_true', help="shows highscores in many categories")
     args = parser.parse_args()
 
     with contextlib.closing(DataLead(DB_NAME, API_KEY)) as DL:
@@ -147,9 +152,11 @@ if __name__ == "__main__":
             print("There are no arguments passed!")
         elif args.download_all:
             DL.download_all_movies()
-        elif args.download_single:
-            DL.download_single_movie(args.download_single)
+        elif args.update_single:
+            DL.update_single_movie(args.update_single)
         elif args.sort_by:
             DL.sort_by(args.sort_by)
         elif args.add:
             DL.add(args.add)
+        elif args.highscores:
+            DL.add(args.highscores)
